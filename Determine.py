3b567bb39aa48a3caa05b34b43de88a1706a1067
Igor Sorokin <igor_sorokin@gap.com>
2015-10-14 15:29:10 -0700

    bunch of new stuff

--- Determine.py
+++ Determine.py
@@ -6,6 +6,11 @@ __status__ = ''
 rank = ["A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2"]
 suit = ["S", "H", "C", "D"]
 
+class Card:
+    def __init__(self, rank, suit):
+        self.rank = rank
+        self.suit = suit
+
 def shuffle():
     import random
     deck = ["" for i in range(1, 53)]
@@ -17,41 +22,42 @@ def shuffle():
             deck[randNum[0]] = r + s
     return deck
 
+def order_by_rank(cards):
+    rank = ["A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2"]
+    cards_dict_by_rank_index = {card: rank.index(card) for card in cards}
+    cards_sorted_by_rank_index = sorted(cards_dict_by_rank_index, key=cards_dict_by_rank_index.get)
+    return cards_sorted_by_rank_index
+
 def determine_combo(cards):
     suit = ["C", "S", "H", "D"]
-    suit_count = dict(zip(suit, [0 for i in range(0, 4)]))
-    #{'H': 0, 'D': 0, 'C': 0, 'S': 0}
+    suit_count = dict(zip(suit, [[] for i in range(0, 4)]))
     rank = ["A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2"]
     rank_count = dict(zip(rank, [0 for i in range(0, 13)]))
-    for card in cards:
-        suit_count[card[1]] += 1
-        rank_count[card[0]] += 1
-
-    suit_count_sorted = sorted(suit_count, key=suit_count.get)
-    suit_count_sorted.reverse()
-    #['S', 'C', 'H', 'D']
     flush_flag = False
-    if suit_count[suit_count_sorted[0]] == 5:
-        flush_flag = True
+    for card in cards:
+        suit_count[card.suit].append(card)
+        rank_count[card.rank] += 1
+        if len(suit_count[card.suit]) == 5:
+            flush_flag = True
+            flush_suit = card.suit
 
-    rank_count = {key: rank_count[key] for key in rank_count if rank_count[key] != 0}
-    rank_count_sorted = sorted(rank_count, key=rank_count.get)
-    rank_count_sorted.reverse()
-
-    Ato5 = [rank[0]]#Exception A,2,3,4,5
-    Ato5.extend(rank[9:13])
+    cards_no_duplicates = {key: rank_count[key] for key in rank_count if rank_count[key] != 0}
+    cards_ordered_by_rank = order_by_rank(cards_no_duplicates.keys())
+    Ato5 = [rank[0]] + rank[9:13]#Exception A,2,3,4,5
     for i in range(0, 9):
-        if sorted(rank_count_sorted[:5]) == sorted(rank[0+i:5+i]) or sorted(rank_count_sorted[:5]) == sorted(Ato5):
+        if cards_ordered_by_rank[:5] == rank[0+i:5+i] or cards_ordered_by_rank[:5] == Ato5:
             if flush_flag:
                 if i is 0:
                     return 10#Royal Flush
                 else:
-                    return [9, rank_count]#Straight Flush
+                    return [9, cards_ordered_by_rank[:5]]#Straight Flush
             else:
-                return [5, rank_count]#Straight
+                return [5, cards_ordered_by_rank[:5]]#Straight
 
     if flush_flag:
-        return [6, rank_count]#Flush
+        flush_cards = [card.rank for card in suit_count[flush_suit]]
+        flush_cards_ordered_by_rank = order_by_rank(flush_cards)
+        return [6, flush_cards_ordered_by_rank]#Flush
 
     if rank_count[rank_count_sorted[0]] == 4:
         return [8, rank_count]#Four of a Kind
@@ -67,14 +73,14 @@ def determine_combo(cards):
         return [1, rank_count_sorted]
 
 def determine_winner(player1_cards, player2_cards):
+    rank = ["A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2"]
     player1_combo = determine_combo(player1_cards)
     player2_combo = determine_combo(player2_cards)
     if player1_combo[0] > player2_combo[0]:
         return 1
     elif player1_combo[0] < player2_combo[0]:
-        return 0
+        return 2
     elif player1_combo[0] == 1:#compare high cards
-        rank = ["A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2"]
         for r in rank:
             for l in range(len(player1_combo[1])):
                 if not (r is player1_combo[1][l] and r is player2_combo[1][l]):#not in both
@@ -91,7 +97,15 @@ def determine_winner(player1_cards, player2_cards):
     elif player1_combo[0] == 5:#compare straight
         misc = 0
     elif player1_combo[0] == 6:#compare flush
-        misc = 0
+        for i in range(len(player1_combo[1])):
+            player1_flush_card_rank = player1_combo[1][i]
+            player2_flush_card_rank = player2_combo[1][i]
+            if player1_flush_card_rank != player2_flush_card_rank:
+                if rank.index(player1_flush_card_rank) < rank.index(player2_flush_card_rank):
+                    return 1
+                else:
+                    return 2
+        return 0#chopped flush
     elif player1_combo[0] == 7:#compare full house
         misc = 0
     elif player1_combo[0] == 8:#compare quads
@@ -111,7 +125,47 @@ def determine_winner(player1_cards, player2_cards):
                     elif r in player2_combo[1].keys():
                         return 0
     '''
+'''
+players = []
+players.append([Card('A', 'H'),
+                Card('2', 'D')])
+players.append([Card('K', 'H'),
+                Card('2', 'C')])
+board = [Card('9', 'H'),
+         Card('8', 'H'),
+         Card('7', 'H'),
+         Card('6', 'H'),
+         Card('3', 'C')]
+print("Resulted: Player " + str(determine_winner(players[0] + board, players[1] + board)))
+print("Expected: Player 1")
 
+players = []
+players.append([Card('Q', 'H'),
+                Card('2', 'D')])
+players.append([Card('K', 'H'),
+                Card('2', 'C')])
+board = [Card('A', 'H'),
+         Card('8', 'H'),
+         Card('7', 'H'),
+         Card('6', 'H'),
+         Card('3', 'C')]
+print("Resulted: Player " + str(determine_winner(players[0] + board, players[1] + board)))
+print("Expected: Player 2")
+'''
+players = []
+players.append([Card('T', 'H'),
+                Card('4', 'C')])
+players.append([Card('2', 'H'),
+                Card('2', 'C')])
+board = [Card('9', 'H'),
+         Card('K', 'H'),
+         Card('Q', 'H'),
+         Card('J', 'H'),
+         Card('3', 'C')]
+print("Resulted: Player " + str(determine_winner(players[0] + board, players[1] + board)))
+print("Expected: Player 2")
+
+'''
 player1_count = 0
 for i in range(10000):
     deck = shuffle()
@@ -126,3 +180,4 @@ for i in range(10000):
     players[1].extend(family)
     print(str(determine_winner(players[0], players[1])))
     player1_count += determine_winner(players[0], players[1])
+'''
